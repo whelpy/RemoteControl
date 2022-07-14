f4972555dd0becfd606ce3f87d215af960406cf0
whelp <woof.timber@gmail.com>
2022-07-14 15:28:28 +0300

    Initial commit

---
+++ RemoteControl/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RemoteControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Remote Control.");
+            var control = new HardwareControl();
+
+        }
+    }
+}
---
+++ RemoteControl/UpdateVisitor.cs
@@ -0,0 +1,22 @@
+﻿using LibreHardwareMonitor.Hardware;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RemoteControl
+{
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
+}
